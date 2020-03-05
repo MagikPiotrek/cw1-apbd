410a8825d24c8d8292cc1ff5692e147a43ac680b
s18921 <s18921@pjwstk.edu.pl>
2020-03-05 09:48:59 +0100

    Commit A Cw1

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,27 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Cw1
 {
+    public class Student
+    {
+        //Wlasciwosc Skrocona
+        public string Nazwisko { get; set; }
+        //propfull
+        private string _imie;
+
+        public string Imie
+        {
+            get { return _imie; }
+            set
+            {
+                if (value == null) throw new ArgumentException();
+                _imie = value;
+            }
+        }
+
+    }
     public class Program
     {
         public static async Task Main(string[] args)
         {
-            var client = new HttpClient();
-            var result = await client.GetAsync("https://www.pja.edu.pl");
-
-            if (result.IsSuccessStatusCode)
+            try
             {
+                var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+
+                var client = new HttpClient();
+                var result = await client.GetAsync("https://www.pja.edu.pl");
+
+                if (!result.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Server Error");
+                    return;
+                }
+
                 string html = await result.Content.ReadAsStringAsync();
                 var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
                 var matches = regex.Matches(html);
-                foreach(var m in matches)
+
+                var set = new HashSet<string>();
+
+                var list = new List<string>();
+
+                var elements = from e in list
+                               where e.StartsWith("A")
+                               select e;
+                var elements2 = list.Where(s => s.StartsWith("A"));
+
+                var slownik = new Dictionary<string, string>();
+
+                foreach (var m in matches)
                 {
                     Console.WriteLine(m);
                 }
+
+            }catch(Exception exc)
+            {
+                //interpolacja stringa
+                //string.Format("Wystapil blad {0}", exc.ToString());
+                Console.WriteLine($"Wystapil blad {exc.ToString()}");
             }
+            
+
 
             Console.WriteLine("KONIEC");
         }
